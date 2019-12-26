b5d806d8d2b7b600cdb1ed82fef6b085bf9644e3
yueqian <2269596725@qq.com>
2019-12-26 22:55:40 +0800

    Initial commit

---
+++ src/main/java/com/bjsxt/springbootjdbc/SpringbootjdbcApplication.java
@@ -0,0 +1,15 @@
+package com.bjsxt.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjdbcApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbootjdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bjsxt/springbootjdbc/SpringbootjdbcApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bjsxt.springbootjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjdbcApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
